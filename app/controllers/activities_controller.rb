1d0ef2fdfedc059f94f5bbadaabc84f338114828
Yves Hurdequint <hurdequinty@hotmail.com>
2021-08-04 18:35:46 +0800

    starting assigning the values to object activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,6 +6,7 @@ class ActivitiesController < ApplicationController
     activity_lists = {"Religion"=>[{"type"=>"location", "subType"=>"POINT_OF_INTEREST", "id"=>"AA8E41776E", "self"=>{"href"=>"https://test.api.amadeus.com/v1/reference-data/locations/pois/AA8E41776E", "methods"=>["GET"]}, "geoCode"=>{"latitude"=>48.852966, "longitude"=>2.349902}, "name"=>"Cathédrale Notre-Dame de Paris", "category"=>"SIGHTS", "rank"=>5, "tags"=>["church", "sightseeing", "restaurant", "tourguide", "sights", "temple", "landmark", "historicplace", "historic", "attraction", "activities", "professionalservices", "sports", "bike", "rental", "commercialplace", "outdoorplace"]}, {"type"=>"location", "subType"=>"POINT_OF_INTEREST", "id"=>"13D98F7D12", "self"=>{"href"=>"https://test.api.amadeus.com/v1/reference-data/locations/pois/13D98F7D12", "methods"=>["GET"]}, "geoCode"=>{"latitude"=>48.84681, "longitude"=>2.337546}, "name"=>"Jardin du Luxembourg", "category"=>"SIGHTS", "rank"=>5, "tags"=>["sightseeing", "restaurant", "tourguide", "sights", "park", "landmark", "historicplace", "historic", "garden", "attraction", "activities", "hiking", "beauty&spas", "theater", "commercialplace", "nature"]}, {"type"=>"location", "subType"=>"POINT_OF_INTEREST", "id"=>"521DE91E65", "self"=>{"href"=>"https://test.api.amadeus.com/v1/reference-data/locations/pois/521DE91E65", "methods"=>["GET"]}, "geoCode"=>{"latitude"=>48.88672, "longitude"=>2.343002}, "name"=>"Basilique du Sacré-Cœur de Montmartre", "category"=>"SIGHTS", "rank"=>5, "tags"=>["church", "sightseeing", "sights", "temple", "landmark", "tourguide", "restaurant", "attraction", "commercialplace", "activities", "professionalservices"]}, {"type"=>"location", "subType"=>"POINT_OF_INTEREST", "id"=>"E565F32CAE", "self"=>{"href"=>"https://test.api.amadeus.com/v1/reference-data/locations/pois/E565F32CAE", "methods"=>["GET"]}, "geoCode"=>{"latitude"=>48.925682, "longitude"=>2.359429}, "name"=>"Stade de France", "category"=>"SIGHTS", "rank"=>5, "tags"=>["stadium", "sightseeing", "tourguide", "landmark", "historicplace", "sights", "historic", "activities", "sports", "events", "shopping", "restaurant", "outdoorplace", "commercialplace", "attraction"]}], "Museum"=>[{"type"=>"location", "subType"=>"POINT_OF_INTEREST", "id"=>"EC7AE15DF5", "self"=>{"href"=>"https://test.api.amadeus.com/v1/reference-data/locations/pois/EC7AE15DF5", "methods"=>["GET"]}, "geoCode"=>{"latitude"=>48.860825, "longitude"=>2.352633}, "name"=>"Centre Pompidou", "category"=>"SIGHTS", "rank"=>5, "tags"=>["museum", "sightseeing", "restaurant", "artgallerie", "tourguide", "sights", "transport", "activities", "attraction", "shopping", "square", "parking", "professionalservices", "bus", "theater", "events", "commercialplace"]}]}
     # looping by category
     open_trip_map(activity_lists)
+    raise
   end
 
   private
@@ -72,6 +73,13 @@ class ActivitiesController < ApplicationController
   def create_activity(activityOTM)
     activity = Activity.new()
     activity.name = activityOTM["name"]
+    activity.link = activityOTM["url"].split(";").first
+    activity.picture_url = activityOTM["preview"]["source"]
 
+    end
+    activity.description = activityOTM["wikipedia_extracts"]["text"]
+    activity.address = "#{activityOTM["address"]["pedestrian"]}, #{activityOTM["address"]["city"]}, #{activityOTM["address"]["country"]}}"
+    activity.latitude = activityOTM["point"]["lat"]
+    activity.longitude = activityOTM["point"]["lon"]
   end
 end

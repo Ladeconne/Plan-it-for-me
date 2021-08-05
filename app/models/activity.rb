124293eeb51a0cfeb340f0a0e4b74394787c4060
Yitang Wang <yitangwang0@gmail.com>
2021-08-05 14:39:06 +0800

    trip destroy

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :trip
   belongs_to :day, optional: true
+  has_many :activity_categories, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?

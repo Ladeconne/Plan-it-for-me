96e06aea93c323d237e40afea610ea1fa06793c2
Yves Hurdequint <hurdequinty@hotmail.com>
2021-08-09 12:41:23 +0800

    reset :current_session on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :clear_session, only: [ :home ]
 
   def home
   end
 
   def components
+  end
+
+  private
+
+  def clear_session
+    session[:current_trip] = nil
   end
 end

846c57faee26fb6f117ce8b9f030e62162b72212
Yitang Wang <yitangwang0@gmail.com>
2021-08-10 10:03:33 +0800

    login and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
 
-  # def after_sign_in_path_for(user)
-  #   if session[:trip_id]
-  #     trip = Trip.find(session[:trip_id])
-  #     trip.user = user
-  #     trip.save
-  #     session.delete(:trip_id)
-  #     trip_path(trip)
-  #   else
-  #     stored_location_for(resource)
-  #   end
-  # end
+    def after_sign_in_path_for(user)
+      if session[:trip_id]
+        trip = Trip.find(session[:trip_id])
+        trip.user = user
+        trip.save
+        session.delete(:trip_id)
+        trip_path(trip)
+      else
+        stored_location_for(resource)
+      end
+  end
 end

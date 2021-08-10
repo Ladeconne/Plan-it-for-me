bf61fe9a44ff1b337b58b447f16379c85564b3fa
jessica kalip <jessicakalip@gmail.com>
2021-08-10 13:53:23 +0800

    Added instrution to activities page

---
+++ app/javascript/components/activity_counter.js
@@ -0,0 +1,10 @@
+const counter = document.querySelector("#counter");
+const activityCheckBoxes = document.querySelectorAll("input[type=checkbox]");
+
+const activityCounter = () => {
+  activityCheckBoxes.addEventListener("click", () => {
+    
+  } 
+};
+
+export { activityCounter };
